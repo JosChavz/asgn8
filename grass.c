2a438f61864a29f265e321d4ccd02bdbf9a69e9a
Jose Chavez <josmchavz@gmail.com>
2021-12-11 08:39:44 -0800

    small edit of binary source. needs so much work

--- grass.c
+++ grass.c
@@ -1,8 +1,27 @@
 #include "bv.h"
 #include "field.h"
+#include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define OPTIONS "aswrvhi:n:s:"
+
 int main(void) {
-	return 0;
+  // Flags
+  int opt = getopt(argc, argv, OPTIONS);
+
+  while (opt != -1) {
+	  switch(opt) {
+		  case 'h':
+			  print_help();
+			  return 0;
+		case 'a':
+			  //tbd
+	  }
+
+	  opt = getopt(argc, argv, OPTIONS);
+  }
+
+  return 0;
 }
