c4f0900f43f4e38cbf92b298b8c1f016f39d21f7
Jose Chavez <josmchavz@gmail.com>
2021-12-11 08:39:21 -0800

    forgot to push?

--- bv.c
+++ bv.c
@@ -6,116 +6,114 @@
 #include <stdlib.h>
 
 typedef struct BitVector {
-	uint32_t length;
-	uint64_t *vector;
-	uint32_t writes;
+  uint32_t length;
+  uint64_t *vector;
+  uint32_t writes;
 } BitVector;
 
 BitVector *bv_create(uint32_t size) {
-	// Alocate memory for bv variables
-	BitVector *bv = (BitVector *) malloc(sizeof(BitVector));
+  // Alocate memory for bv variables
+  BitVector *bv = (BitVector *)malloc(sizeof(BitVector));
 
-	if (bv != NULL) {
-		bv->length = 0;
-		bv->writes = 0;
-		// Allocate memory for vector
-		// n x n BITS in vector, thus allocation is size / 64
-		// to know exactly the length of the vector
-		bv->vector = (uint64_t *) calloc(size / 64, sizeof(uint64_t));
+  if (bv != NULL) {
+    bv->length = 0;
+    bv->writes = 0;
+    // Allocate memory for vector
+    // n x n BITS in vector, thus allocation is size / 64
+    // to know exactly the length of the vector
+    bv->vector = (uint64_t *)calloc(size / 64, sizeof(uint64_t));
 
-		if(bv->vector == NULL) {
-			// Unable to allocate memory
-			free(bv);
-			bv = NULL;
-		}
-	}
+    if (bv->vector == NULL) {
+      // Unable to allocate memory
+      free(bv);
+      bv = NULL;
+    }
+  }
 
-	return bv;
+  return bv;
 }
 
 void bv_delete(BitVector **bv) {
-	// Delete bv
-	if(bv != NULL) {
-		free((*bv)->vector);
-		free(*bv);
-		*bv = NULL;
-	}
+  // Delete bv
+  if (bv != NULL) {
+    free((*bv)->vector);
+    free(*bv);
+    *bv = NULL;
+  }
 
-	return;
+  return;
 }
 
-uint32_t bv_length(BitVector *bv) {
-	return bv->length;
-}
+uint32_t bv_length(BitVector *bv) { return bv->length; }
 
 bool bv_set_bit(BitVector *bv, uint32_t i) {
-	if (bv && i < bv->length) {
-		bv->vector[i/64] |= (uint64_t) 0x1 << i % 64;
-		bv->writes += 1;
-		return true;
-	}
+  if (bv && i < bv->length) {
+    bv->vector[i / 64] |= (uint64_t)0x1 << i % 64;
+    bv->writes += 1;
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
 bool bv_clr_bit(BitVector *bv, uint32_t i) {
-	if (bv && i < bv->length) {
-		bv->vector[i / 64] &= ~((uint64_t) 0x1 << (i % 64));
-		bv->writes += 1;
-		return true;
-	}
+  if (bv && i < bv->length) {
+    bv->vector[i / 64] &= ~((uint64_t)0x1 << (i % 64));
+    bv->writes += 1;
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
 bool bv_get_bit(BitVector *bv, uint32_t i, bool *bit) {
-	if (bv && i < bv->length) {
-		// Firstly grabs the bit that resides in i-position
-		// Then moves that bit to the LSB
-		uint64_t curr_bit = (bv->vector[i / 64] & (uint64_t) 0x1 << i % 64) >> i % 64;
-		*bit = curr_bit;
+  if (bv && i < bv->length) {
+    // Firstly grabs the bit that resides in i-position
+    // Then moves that bit to the LSB
+    uint64_t curr_bit =
+        (bv->vector[i / 64] & (uint64_t)0x1 << i % 64) >> i % 64;
+    *bit = curr_bit;
 
-		return true;
-	}
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
 bool bv_set_64(BitVector *bv, uint32_t i) {
-	if (bv && i < bv->length) {
-		bv->vector[i/64] |= ~((uint64_t) 0);
-		bv->writes += 1;
-		return true;
-	}
+  if (bv && i < bv->length) {
+    bv->vector[i / 64] |= ~((uint64_t)0);
+    bv->writes += 1;
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
 bool bv_clr_64(BitVector *bv, uint32_t i) {
-	if (bv->vector) {
-		bv->vector[i / 64] &= (uint64_t) 0;
-		bv->writes += 1;
-		return true;
-	}
+  if (bv->vector) {
+    bv->vector[i / 64] &= (uint64_t)0;
+    bv->writes += 1;
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
 uint32_t bv_writes(BitVector *bv) {
-	if (bv) {
-		return bv->writes;
-	}
+  if (bv) {
+    return bv->writes;
+  }
 
-	return 0;
+  return 0;
 }
 
 void bv_print(BitVector *bv) {
-    printf("%c ", '[');
-    for (uint32_t i = 0; i < bv->length; i++) {
-		bool temp_b = false;
-        bool c_bit = bv_get_bit(bv, i, &temp_b);
-        printf("%d ", c_bit);
-    }
-    printf("%c\n", ']');
+  printf("%c ", '[');
+  for (uint32_t i = 0; i < bv->length; i++) {
+    bool temp_b = false;
+    bool c_bit = bv_get_bit(bv, i, &temp_b);
+    printf("%d ", c_bit);
+  }
+  printf("%c\n", ']');
 }
-
--- field.c
+++ field.c
@@ -1,5 +1,5 @@
-#include "bv.h"
 #include "field.h"
+#include "bv.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -7,107 +7,103 @@
 #include <stdlib.h>
 
 typedef struct Field {
-	uint32_t size;
-	BitVector *matrix;
+  uint32_t size;
+  BitVector *matrix;
 } Field;
 
 Field *field_create(uint32_t size) {
-	Field *field = malloc(sizeof(Field));
+  Field *field = malloc(sizeof(Field));
 
-	if (field != NULL) {
-        field->size = size;
-        field->matrix = (BitVector *)  bv_create(size * size);
+  if (field != NULL) {
+    field->size = size;
+    field->matrix = (BitVector *)bv_create(size * size);
 
-		if (field->matrix == NULL) {
-			free(field);
-			field = NULL;
-		}
-	}
+    if (field->matrix == NULL) {
+      free(field);
+      field = NULL;
+    }
+  }
 
-	return field;
+  return field;
 }
 
 void field_delete(Field **f) {
-	if (f) {
-		bv_delete(&(*f)->matrix);
-		free(*f);
-		f = NULL;
-	}
+  if (f) {
+    bv_delete(&(*f)->matrix);
+    free(*f);
+    f = NULL;
+  }
 
-	return;
+  return;
 }
 
-uint32_t field_size(Field *f) {
-	return f->size;
-}
+uint32_t field_size(Field *f) { return f->size; }
 
-uint32_t field_area(Field *f) {
-	return f->size * f->size;
-}
+uint32_t field_area(Field *f) { return f->size * f->size; }
 
 uint32_t field_count(Field *f) {
-	if (f) {
-		uint32_t count = 0;
+  if (f) {
+    uint32_t count = 0;
 
-		// For Matrix count
-		for (uint32_t i = 0; i < field_area(f); i += 1) {
-			bool temp_b = false;
+    // For Matrix count
+    for (uint32_t i = 0; i < field_area(f); i += 1) {
+      bool temp_b = false;
 
-			if (bv_get_bit(f->matrix, i, &temp_b) && temp_b) {
-				count += 1;
-			}
-		}
+      if (bv_get_bit(f->matrix, i, &temp_b) && temp_b) {
+        count += 1;
+      }
+    }
 
-		return count;
-	}
+    return count;
+  }
 
-	return 0;
+  return 0;
 }
 
 uint32_t field_writes(Field *f) {
-	if (f) {
-		return bv_writes(f->matrix);
-	}
-	
-	return 0;
+  if (f) {
+    return bv_writes(f->matrix);
+  }
+
+  return 0;
 }
 
 void field_touch_sequential(Field *f, uint32_t max_iters, unsigned int seed) {
-	(void) seed;
+  (void)seed;
 
-	if (f && max_iters < field_area(f)) {
-		for (uint32_t i = 0; i < max_iters; i += 1) {
-			bv_set_bit(f->matrix, i);
-		}
+  if (f && max_iters < field_area(f)) {
+    for (uint32_t i = 0; i < max_iters; i += 1) {
+      bv_set_bit(f->matrix, i);
+    }
 
-		return; 
-	}
+    return;
+  }
 
-	return;
+  return;
 }
 
 void field_touch_wide(Field *f, uint32_t max_iters, unsigned int seed) {
-	(void) seed;
+  (void)seed;
 
-	if (f && max_iters < field_area(f)) {
-		for (uint32_t i = 0; i < max_iters; i += 1) {
-			bv_set_64(f->matrix, i);
-		}
-	}
+  if (f && max_iters < field_area(f)) {
+    for (uint32_t i = 0; i < max_iters; i += 1) {
+      bv_set_64(f->matrix, i);
+    }
+  }
 
-	return;
+  return;
 }
 
 void field_touch_random(Field *f, uint32_t max_iters, unsigned int seed) {
-	if (f && max_iters < field_area(f)) {
-		// Sets the random seed
-		srandom(seed);
-		for (uint32_t i = 0; i < max_iters; i += 1) {
-			bv_set_64(f->matrix, random() % field_area(f));
-		}
-	}
+  if (f && max_iters < field_area(f)) {
+    // Sets the random seed
+    srandom(seed);
+    for (uint32_t i = 0; i < max_iters; i += 1) {
+      bv_set_64(f->matrix, random() % field_area(f));
+    }
+  }
 
-	return;
+  return;
 }
 
 void field_print(Field *f);
