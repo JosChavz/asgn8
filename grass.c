538fbedff915f8f807f425d505c0eefe4fde20c0
Jose Chavez <josmchavz@gmail.com>
2021-12-12 11:18:01 -0800

    fixing bugs in options

--- grass.c
+++ grass.c
@@ -5,10 +5,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
 
 #define OPTIONS "aSwrvhi:n:s:"
-#define SEED 756670
-#define SIZE 10
+#define DEFAULT_SEED 756670
+#define DEFAULT_SIZE 10
 #define MAX_SIZE 1024
 
 static inline void print_help();
@@ -20,10 +22,11 @@ typedef enum {
   HELP
 } static bool verbose = false;
 
-int main(void) {
+int main(int argc, char **argv) {
   // Initializing
-  uint32_t seed = 7566707;
+  uint32_t seed = DEFAULT_SEED;
   uint32_t iters = 10;
+  uint32_t size = DEFAULT_SIZE;
   Set s = 0;
 
   // Flags
@@ -61,7 +64,6 @@ int main(void) {
           // Argument is a value but less than 0 or greater than the unsigned
           // max
           puts("Out of range. Setting iters size as default.");
-          iters = 25;
         } else {
           // Value passed is valid
 	  iters = temp_iters * temp_iters;
@@ -70,45 +72,44 @@ int main(void) {
       break;
     case 'n':
       if (optarg != NULL) {
-        long temp_iters = strtol(optarg, NULL, 10);
+        long temp_size = strtol(optarg, NULL, 10);
         // Argument is invalid, meaning not an integer
         if (errno == EINVAL) {
           // The argument is another flag
           if (optarg[0] == '-') {
             optind--;
           } else { // Some invalid string
-            puts("Invalid input. Setting iters size as default.");
+            puts("Invalid input. Setting size as default.");
           }
-        } else if (temp_iters > UINT_MAX || temp_iters < 0) {
+        } else if (temp_size > MAX_SIZE || temp_size < 1) {
           // Argument is a value but less than 0 or greater than the unsigned
           // max
-          puts("Out of range. Setting iters size as default.");
-          iters = 25;
+          puts("Out of range. Setting size as default.");
         } else {
           // Value passed is valid
-	  iters = temp_iters * temp_iters;
+	  size = temp_size;
         }
       }
       break;
     case 'S':
       if (optarg != NULL) {
-        long temp_iters = strtol(optarg, NULL, 10);
+        long temp_seed = strtol(optarg, NULL, 10);
+
         // Argument is invalid, meaning not an integer
         if (errno == EINVAL) {
           // The argument is another flag
           if (optarg[0] == '-') {
             optind--;
           } else { // Some invalid string
-            puts("Invalid input. Setting iters size as default.");
+            puts("Invalid input. Setting seed size as default.");
           }
-        } else if (temp_iters > UINT_MAX || temp_iters < 0) {
+        } else if (temp_seed > UINT_MAX || temp_seed < 0) {
           // Argument is a value but less than 0 or greater than the unsigned
           // max
-          puts("Out of range. Setting iters size as default.");
-          iters = 25;
+          puts("Out of range. Setting seed size as default.");
         } else {
           // Value passed is valid
-	  iters = temp_iters * temp_iters;
+	  seed = temp_seed;
         }
       }
       break;
